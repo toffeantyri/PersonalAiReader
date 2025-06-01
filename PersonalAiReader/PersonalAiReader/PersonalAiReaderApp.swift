043b50b10b3b33b8e20f187db104f6aff3442b98
toffeantyri <toffeatyri@gmail.com>
2025-06-02 00:20:03 +0500

    add empty ios application

---
+++ PersonalAiReader/PersonalAiReader/PersonalAiReaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  PersonalAiReaderApp.swift
+//  PersonalAiReader
+//
+//  Created by Family on 02.06.2025.
+//
+
+import SwiftUI
+
+@main
+struct PersonalAiReaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
--- iosApp/build.gradle.kts
+++ iosApp/build.gradle.kts
@@ -1,3 +1,7 @@
+import org.jetbrains.kotlin.gradle.plugin.mpp.KotlinNativeTarget
+import org.jetbrains.kotlin.gradle.plugin.mpp.NativeBuildType
+import org.jetbrains.kotlin.konan.target.Family
+
 plugins {
     alias(libs.plugins.compose)
     alias(libs.plugins.compose.compiler)
@@ -7,6 +11,30 @@ plugins {
 kotlin {
     iosX64()
     iosArm64()
+
+    targets
+        .filterIsInstance<KotlinNativeTarget>()
+        .filter { it.konanTarget.family == Family.IOS }
+        .forEach {
+            it.binaries.framework(listOf(NativeBuildType.DEBUG)) {
+                binaryOption("bundleId", "notebook.ai.reader")
+                baseName = "shared"
+                isStatic = true
+
+                //decompose
+                export(libs.decompose.decompose)
+                export(libs.decompose.extensionsComposeJetbrains)
+
+                export(libs.essenty.lifecycle)
+
+
+                // Optional, only if you need state preservation on Darwin (Apple) targets
+                //export("com.arkivanov.essenty:state-keeper:<essenty_version>")
+
+                // Optional, only if you need state preservation on Darwin (Apple) targets
+                //export("com.arkivanov.parcelize.darwin:runtime:<parcelize_darwin_version>")
+            }
+        }
 
     sourceSets {
         val commonMain by getting {
