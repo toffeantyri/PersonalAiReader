1d2d39c4f074c9e31e0098691f0eb56c9171ddfd
toffeantyri <toffeatyri@gmail.com>
2025-06-03 00:36:35 +0500

    run on ios - success

--- shared/src/iosMain/kotlin/ai/personal/reader/MainViewController.kt
+++ shared/src/iosMain/kotlin/ai/personal/reader/MainViewController.kt
@@ -1,15 +1,33 @@
 package ai.personal.reader
 
 import ai.personal.reader.theme.AppTheme
-import ai.personal.reader.ui.screens.DefaultScreen
+import ai.personal.reader.ui.components.root.RootComponentImpl
+import ai.personal.reader.ui.screens.RootContent
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.window.ComposeUIViewController
+import com.arkivanov.decompose.DefaultComponentContext
+import com.arkivanov.essenty.lifecycle.LifecycleRegistry
+import com.arkivanov.essenty.statekeeper.StateKeeperDispatcher
+import platform.UIKit.UIViewController
 
 @Composable
 fun App() {
+    val lifecycle = remember { LifecycleRegistry() }
+    val stateKeeper = remember { StateKeeperDispatcher() }
+
+    val rootComponent = remember {
+        RootComponentImpl(
+            componentContext = DefaultComponentContext(
+                lifecycle = lifecycle,
+                stateKeeper = stateKeeper
+            )
+        )
+    }
+
     AppTheme {
-        DefaultScreen()
+        RootContent(rootComponent)
     }
 }
 
-fun MainViewController() = ComposeUIViewController { App() }
+fun MainViewController(): UIViewController = ComposeUIViewController { App() }
